b9880884e19e81cb60c07fde6794300f1be845c8
Mikroh <mikael.holsve@live.dk>
2015-03-07 17:09:43 +0100

    Added SQL statements, in the case of broken database

---
+++ SQL_statements(NOT_USED)/dbo.Videos.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Videos] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Title]  NVARCHAR (50) NOT NULL,
+    [URL]    NVARCHAR (50) NOT NULL,
+    [userID] INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Videos_Users] FOREIGN KEY ([userID]) REFERENCES [dbo].[Users] ([Id])
+);
+
