f6a522a0a95cce867d202827cdef376a38b0c65d
Mikroh <mikael.holsve@live.dk>
2015-03-08 03:28:59 +0100

    You can no longer add videos that have already exist

--- my_account.aspx.cs
+++ my_account.aspx.cs
@@ -33,23 +33,36 @@ public partial class my_account : System.Web.UI.Page
         TextBoxURL.Text = TextBoxURL.Text.Replace("https://www.youtube.com/watch?v=", "");
         TextBoxURL.Text = TextBoxURL.Text.Replace("http://www.youtube.com/watch?v=", "");
         TextBoxURL.Text = TextBoxURL.Text.Replace("www.youtube.com/watch?v=", "");
+        TextBoxURL.Text = TextBoxURL.Text.Replace("youtube.com/watch?v=", "");
+        TextBoxURL.Text = TextBoxURL.Text.Replace("http://youtube.com/watch?v=", "");
+        TextBoxURL.Text = TextBoxURL.Text.Replace("https://youtube.com/watch?v=","");
 
         SqlConnection con = new SqlConnection();
         con.ConnectionString = ConfigurationManager.ConnectionStrings["DatabaseConnectionString1"].ToString();
         SqlCommand cmd = new SqlCommand();
         cmd.Connection = con;
-        cmd.CommandText = "INSERT INTO Videos (title, URL, userID) VALUES (@title, @URL, @userID)";
+        cmd.CommandText = "UPDATE Videos SET title=title WHERE URL = @URL IF @@ROWCOUNT=0 INSERT INTO Videos (title, URL, userID) VALUES (@title, @URL, @userID)";
 
         cmd.Parameters.AddWithValue("@title", TextBoxTitle.Text);
         cmd.Parameters.AddWithValue("@URL", TextBoxURL.Text);
         cmd.Parameters.AddWithValue("@userID", Session["id"]);
 
         con.Open();
-        cmd.ExecuteNonQuery();
+        int result = cmd.ExecuteNonQuery();
         con.Close();
 
+        if(result != 1) {
+            TextBoxURL.Text = "";
+            LabelSuccess.Text = "Der var en fejl. Din video er måske allerede blevet tilføjet.";
+            LabelSuccess.ForeColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+            LabelSuccess.Visible = true;
+        }
+        else {
         TextBoxTitle.Text = "";
         TextBoxURL.Text = "";
+        LabelSuccess.Text = "Le upload is complete";
+        LabelSuccess.ForeColor = System.Drawing.ColorTranslator.FromHtml("#00E31A");
         LabelSuccess.Visible = true;
-    }
+        }
+        }
 }
